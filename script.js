d52de8f163bad096efb7021cbe6e07263e4b8da5
Towfeeq Khan <ask.towfeeq@gmail.com>
2025-04-11 21:16:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-menu-line" : "ri-close-line");
+  if (isOpen) {
+    navLinks.classList.add("close");
+    navLinks.addEventListener(
+      "animationend",
+      (e) => {
+        navLinks.classList.remove("open");
+        navLinks.classList.remove("close");
+      },
+      { once: true }
+    );
+  } else {
+    navLinks.classList.add("open");
+  }
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// Hero section animations
+ScrollReveal().reveal(".header__container .section__subheader", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__container .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+// Package cards animation
+ScrollReveal().reveal(".package__card", {
+  ...scrollRevealOption,
+  origin: "bottom",
+  delay: 500,
+});
